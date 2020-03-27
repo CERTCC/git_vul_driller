0351ed7e50d8032d2b704bbabdca93cca447de9f
Allen D. Householder <adh@cert.org>
2020-03-27 09:26:18 -0400

    initial commit, nothing works

---
+++ demo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''
+file: demo.py
+author: adh
+created_at: 3/27/20 8:19 AM
+'''
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ git_repo_crawler/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''
+file: __init__.py
+author: adh
+created_at: 3/27/20 8:12 AM
+'''
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
