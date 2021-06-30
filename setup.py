6c223df69d63ed79d50717c65f01b6755e591810
Allen D. Householder <adh@cert.org>
2021-06-30 15:18:42 -0400

    add installable script

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(
-        name='git_repo_crawler',
-        version='0.1',
-        packages=['git_repo_crawler'],
-        url='https://vuls.cert.org',
-        license='',
-        author='adh',
-        author_email='adh@cert.org',
-        description='Crawl the log history of a git repo and find commits matching a regex'
+    name="git_repo_crawler",
+    version="0.1",
+    packages=["git_repo_crawler"],
+    url="https://vuls.cert.org",
+    license="",
+    author="adh",
+    author_email="adh@cert.org",
+    description="Crawl the log history of a git repo and find commits matching a regex",
+    scripts=["scripts/update_exploits",],
 )
