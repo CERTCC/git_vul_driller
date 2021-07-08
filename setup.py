b8714859efe19e4489c750767112b57ffec5eb9e
Allen D. Householder <adh@cert.org>
2021-07-08 15:58:43 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@ from setuptools import setup
 
 setup(
     name="git_vul_driller",
-    version="0.1",
+    version="0.3",
     packages=["git_vul_driller"],
     url="https://vuls.cert.org",
     license="",
     author="adh",
     author_email="adh@cert.org",
     description="Crawl the log history of a git repo and find commits matching a regex",
-    scripts=[
-        "scripts/update_exploits",
-    ],
+    scripts=["scripts/update_exploits",],
 )
